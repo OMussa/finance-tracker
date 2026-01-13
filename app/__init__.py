708b83e9db7b6d2d9171e5d29221223cf7ae18a5
OMussa <mussa_o@icloud.com>
2026-01-13 15:46:36 -0600

    wired SQLAlchemy and Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 """
 importing the Flask class to handle path/routes
 """
-from flask import Flask , render_template
+from flask import Flask 
 from app.auth.routes import authBlueprint
 from app.main.routes import main
 from app.transactions.routes import transactions
+from app.extensions import db , migrate
 
 
 
-
+#creating flask app instance
 app = Flask(
     __name__,
     
     template_folder="../templates")
+#loading config 
 app.config.from_object("config.Config")
+#database setup
+db.init_app(app)
+migrate.init_app(app,db)
     
 app.register_blueprint(authBlueprint)
 app.register_blueprint(main)
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import os 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-not-secure") 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///finance_tracker.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
